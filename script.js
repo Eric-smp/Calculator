d6600874be9aa9f500f7919b153261f4185d89cb
Eric-smp <ericsmp18@gmail.com>
2023-04-10 16:04:36 -0300

    Melhoria do código

---
+++ script.js
@@ -0,0 +1,23 @@
+var numone = document.getElementById("numone")
+var numtwo = document.getElementById("numtwo")
+var result = document.getElementById("resultado")
+
+function soma(){
+  result.innerHTML = numone + numtwo
+  const soma = numone + numtwo
+  console.log("resultado da soma", soma)
+}
+
+function subtracao(){
+  const subtracao = numone - numtwo
+  console.log("resultado da subtração", subtracao)
+}
+function divisao(){
+  const divisao = numone / numtwo
+  console.log("O resultado da divisão é", divisao)
+}
+
+function multiplicacao(){
+  const multiplicacao = numone * numtwo
+  console.log("O resultado da multiplicação é ", multiplicacao)
+}
