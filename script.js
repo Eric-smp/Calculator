0e6a56730c68d56271befc50205b698c1ee01770
Eric-smp <ericsmp18@gmail.com>
2023-04-11 17:41:42 -0300

    Criação para o novo projeto com um input

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const teste = '5  -  5'
+const subtra = teste.slice(1,4)
+const testeOne = '5  +  5'
+const soma = testeOne.slice(1,5)
+const testeTwo = '5  /  5'
+const divisao = testeTwo.slice(1,5)
+const testeTree = '5  *  5'
+const multi = testeTree.slice(1,5)
+
+const numone = document.getElementById('nomone').value
+
 function result(){
   
 
+
+  if(subtra){
+    console.log (document.getElementById('resultado').innerHTML = parseFloat(numone, 10) )
+  }
 }
