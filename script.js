0c8c0e6ab38150b272ec53acec8a964d32d850f2
Eric-smp <ericsmp18@gmail.com>
2023-04-11 12:12:27 -0300

    alteração no java e html para um unico input

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-function somar(){
-  const numone = document.getElementById('numone').value
-  const numtwo = document.getElementById('numtwo').value
-  document.getElementById('resultado').innerHTML= parseFloat(numone, 10)+ parseFloat(numtwo, 10)
-}
-
-function subtracao(){
-  const numone = document.getElementById('numone').value
-  const numtwo = document.getElementById('numtwo').value
-  document.getElementById('resultado').innerHTML= parseFloat(numone, 10) - parseFloat(numtwo, 10)
-}
-function divisao(){
-  const numone = document.getElementById('numone').value
-  const numtwo = document.getElementById('numtwo').value
-  document.getElementById('resultado').innerHTML= parseFloat(numone, 10) / parseFloat(numtwo, 10)
-}
-
-function multiplicacao(){
-  const numone = document.getElementById('numone').value
-  const numtwo = document.getElementById('numtwo').value
-  document.getElementById('resultado').innerHTML= parseFloat(numone, 10) * parseFloat(numtwo, 10)
+function result(){
+  
 }
