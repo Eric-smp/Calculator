e5a4fc4961ad3b1cdcc0787b4145d6583ca302f2
Eric-smp <ericsmp18@gmail.com>
2023-04-12 09:16:01 -0300

    Operação de matématica com um input

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const teste = '5  -  5'
-const subtra = teste.slice(1,4)
+/* const teste = '5-5'
+const subtra = teste.
 const testeOne = '5  +  5'
 const soma = testeOne.slice(1,5)
 const testeTwo = '5  /  5'
 const divisao = testeTwo.slice(1,5)
 const testeTree = '5  *  5'
-const multi = testeTree.slice(1,5)
+const multi = testeTree.slice(1,5) */
 
-const numone = document.getElementById('nomone').value
+
 
 function result(){
-  
-
-
-  if(subtra){
-    console.log (document.getElementById('resultado').innerHTML = parseFloat(numone, 10) )
+  const numone = document.getElementById('numone').value
+  const opera = numone.split(' ')[1]
+  const n1 = numone.split(' ')[0]
+  const n2 = numone.split(' ')[2]
+  console.log(opera)
+  if (opera === '+'){
+    document.getElementById('resultado').innerHTML= parseFloat(n1) + parseFloat(n2)
   }
+  if(opera === '-'){
+    document.getElementById('resultado').innerHTML= parseFloat(n1) - parseFloat(n2)
+  }
+  if(opera === 'x' || opera === '*'){
+    document.getElementById('resultado').innerHTML= parseFloat(n1) * parseFloat(n2)
+  }
+  if(opera === '/'){
+    document.getElementById('resultado').innerHTML= parseFloat(n1) / parseFloat(n2)
+  }
+  
 }
